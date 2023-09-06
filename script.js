64505c36056226d6406223369f4d2cd724eb12a2
josé <josepedrostenzel@gmail.com>
2023-09-06 12:15:05 -0300

    ccc
    cc

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+$('#btnclique').click(function(){
+
+   alert('😮');
+
+   var nome = prompt ('( •_•)>⌐■-■')
+       alert (nome);
+      
+    $('mensagem').html ('<div class="alert alert-dark" role="alert"> +nome+ </div>')   
+
+});
+
+});
